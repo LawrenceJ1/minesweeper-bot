d6a71fd5355b80d16787434aca9d08ea744aae37
LawrenceJ1 <lawrencejuottawa@gmail.com>
2022-07-11 21:05:13 -0400

    refactor code

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import pyautogui
 
 #CURRENTLY ONLY WORKS FOR 200% ZOOM ON 1080x1920 MONITOR
 
-# RGB Values for every number
-colors = [(255, 255, 255), (0, 0, 255), (0, 123, 0), (255, 0, 0), (0, 0, 123), (0, 123, 123), (0, 0, 0), (123, 123, 123), (300, 300, 300)]
 # UNCHECKED = (255, 255, 255), top left will be checked for this
 # ONE = (0, 0, 255)
 # TWO = (0, 123, 0)
@@ -15,87 +13,125 @@ colors = [(255, 255, 255), (0, 0, 255), (0, 123, 0), (255, 0, 0), (0, 0, 123), (
 # SEVEN = (0, 0, 0)
 # EIGHT = (123, 123, 123)
 
-boxes = []
-visited = set()
-
-for i in pyautogui.locateAllOnScreen("square.png", confidence=0.98):
-    _, _, box_width, box_height = i
-    z = pyautogui.center(i)
-    x, y = z
-    boxes.append([x+3, int(y-0.25*box_height)])
-
-#determining the difficulty
-if len(boxes) == 81:
-    model = [[0 for _ in range(9)] for _ in range(9)]
-    mines = 10
-elif len(boxes) == 256:
-    model = [[0 for _ in range(16)] for _ in range(16)]
-    mines = 40
-else:
-    model = [[0 for _ in range(30)] for _ in range(16)]
-    mines = 99
-
-#the game will start with a click on the top left box
-pyautogui.click(x=boxes[0][0], y=boxes[0][1])
-
 # The numbers 1-8 represent the number of mines around a square, while the number -1 is an unchecked square and the number
 # 9 is a checked square that has no more mines around it. The number 10 will represent a flag
 
-#gets a model for the board
-row, col = 0, 0
-m, n = len(model), len(model[0])
-tolerance = 5
-for box in boxes:
-    if (box[0], box[1]) in visited:
-        continue
-    pix = pyautogui.pixel(int(box[0]), int(box[1]))
-    for i in range(10):
-        if i == 0:
-            if pyautogui.pixel(int(box[0]-3-box_width/2), int(box[1])) == colors[i]:
-                model[row][col] = -1
-                break
-        elif i == 9:
-            model[row][col] = i
-            visited.add((box[0], box[1]))
-            break
-        elif (abs(pix[0]-colors[i][0]) <= tolerance) and (abs(pix[1]-colors[i][1]) <= tolerance) and (abs(pix[2]-colors[i][2]) <= tolerance):
-            model[row][col] = i
-            visited.add((box[0], box[1]))
-            break
-    col += 1
-    if col == n:
-        row += 1
-        col = 0
+class Program:
+    def __init__(self) -> None:
+        # RGB Values for every number
+        self.colors = [(255, 255, 255), (0, 0, 255), (0, 123, 0), (255, 0, 0), (0, 0, 123), (0, 123, 123), (0, 0, 0), (123, 123, 123), (300, 300, 300)]
+        self.boxes = []
+        self.visited = set()
+        self.box_width = 0
+        self.box_height = 0
+        self.mines = 0
+        self.model = [[]]
 
-#check for obvious flags
-for i in range(len(model)):
-    for j in range(len(model[i])):
-        if model[i][j] != -1 and model[i][j] != 9 and model[i][j] != 10:
-            sqaures_around = 0
-            squares = []
-            for x in range(-1, 2):
-                for y in range(-1, 2):
-                    if i+x >= 0 and j+y >= 0 and i+x < len(model) and j+y < len(model[0]) and model[i+x][j+y] == -1:
-                        sqaures_around += 1
-                        squares.append([i+x, j+y])
-            if sqaures_around == model[i][j]:
-                for square in squares:
-                    model[square[0]][square[1]] = 10
-                    pyautogui.rightClick(boxes[square[0]*len(model[0])+square[1]][0], boxes[square[0]*len(model[0])+square[1]][1])
-                    mines -= 1
-                    visited.add((boxes[square[0]*len(model[0])+square[1]][0], boxes[square[0]*len(model[0])+square[1]][1]))
+    def initializeBoard(self) -> None:
+        """Gets the position of each of the boxes on the minesweeper board, as well as initializes the model and the number of mines"""
+        for i in pyautogui.locateAllOnScreen("square.png", confidence=0.98):
+            _, _, self.box_width, self.box_height = i
+            z = pyautogui.center(i)
+            x, y = z
+            self.boxes.append([x+3, int(y-0.27*self.box_height)])
+
+        #determining the difficulty
+        if len(self.boxes) == 81:
+            self.model = [[0 for _ in range(9)] for _ in range(9)]
+            self.mines = 10
+        elif len(self.boxes) == 256:
+            self.model = [[0 for _ in range(16)] for _ in range(16)]
+            self.mines = 40
+        else:
+            self.model = [[0 for _ in range(30)] for _ in range(16)]
+            self.mines = 99
+
+    def startGame(self) -> None:
+        """the game will start with a click on the top left box"""
+        pyautogui.click(x=self.boxes[0][0], y=self.boxes[0][1])
+
+
+    def getModel(self) -> None:
+        """Updates the model for the board"""
+        row, col = 0, 0
+        m, n = len(self.model), len(self.model[0]) #REMINDER: use these variables instead of len()
+        tolerance = 5
+        for box in self.boxes:
+            if (box[0], box[1]) in self.visited:
+                col += 1
+                if col == n:
+                    row += 1
+                    col = 0
+                continue
+            pix = pyautogui.pixel(int(box[0]), int(box[1]))
+            for i in range(10):
+                if i == 0:
+                    if pyautogui.pixel(int(box[0]-3-self.box_width/2), int(box[1])) == self.colors[i]:
+                        self.model[row][col] = -1
+                        break
+                elif i == 9:
+                    self.model[row][col] = i
+                    self.visited.add((box[0], box[1]))
+                    break
+                elif (abs(pix[0]-self.colors[i][0]) <= tolerance) and (abs(pix[1]-self.colors[i][1]) <= tolerance) and (abs(pix[2]-self.colors[i][2]) <= tolerance):
+                    self.model[row][col] = i
+                    self.visited.add((box[0], box[1]))
+                    break
+            col += 1
+            if col == n:
+                row += 1
+                col = 0
+
+    def placeObviousFlags(self) -> None:
+        """Places a flag where there is only one possible location that a mine could be in."""
+        for i in range(len(self.model)):
+            for j in range(len(self.model[i])):
+                if self.model[i][j] != -1 and self.model[i][j] != 9 and self.model[i][j] != 10:
+                    sqaures_around = 0
+                    squares = []
                     for x in range(-1, 2):
                         for y in range(-1, 2):
-                            if square[0]+x >= 0 and square[1]+y >= 0 and square[0]+x < len(model) and square[1]+y < len(model[0]) and model[square[0]+x][square[1]+y] != -1 and model[square[0]+x][square[1]+y] != 9 and model[square[0]+x][square[1]+y] != 10:
-                                model[square[0]+x][square[1]+y] -= 1
-                                
-#click squares where there are no more mines
-for row in range(len(model)):
-    for col in range(len(model[col])):
-        if model[row][col] == 0:
-            for x in range(-1, 2):
-                for y in range(-1, 2):
-                    if row+x >= 0 and col+y >= 0 and row+x < len(model) and col+y < len(model[0]) and (boxes[(row+x)*len(model[0])+col+y][0], boxes[(row+x)*len(model[0])+col+y][1]) not in visited:
-                        pyautogui.click(boxes[(row+x)*len(model[0])+col+y][0], boxes[(row+x)*len(model[0])+col+y][1])
-                        visited.add((boxes[(row+x)*len(model[0])+col+y][0], boxes[(row+x)*len(model[0])+col+y][1]))
-                        
+                            if i+x >= 0 and j+y >= 0 and i+x < len(self.model) and j+y < len(self.model[0]) and self.model[i+x][j+y] == -1:
+                                sqaures_around += 1
+                                squares.append([i+x, j+y])
+                    if sqaures_around == self.model[i][j]:
+                        for square in squares:
+                            self.model[square[0]][square[1]] = 10
+                            pyautogui.rightClick(self.boxes[square[0]*len(self.model[0])+square[1]][0], self.boxes[square[0]*len(self.model[0])+square[1]][1])
+                            self.mines -= 1
+                            self.visited.add((self.boxes[square[0]*len(self.model[0])+square[1]][0], self.boxes[square[0]*len(self.model[0])+square[1]][1]))
+                            for x in range(-1, 2):
+                                for y in range(-1, 2):
+                                    if square[0]+x >= 0 and square[1]+y >= 0 and square[0]+x < len(self.model) and square[1]+y < len(self.model[0]) and self.model[square[0]+x][square[1]+y] != -1 and self.model[square[0]+x][square[1]+y] != 9 and self.model[square[0]+x][square[1]+y] != 10:
+                                        self.model[square[0]+x][square[1]+y] -= 1
+    
+    def clickEmptySquares(self) -> None:           
+        """Click squares where there are no more mines."""
+        for row in range(len(self.model)):
+            for col in range(len(self.model[row])):
+                if self.model[row][col] == 0:
+                    for x in range(-1, 2):
+                        for y in range(-1, 2):
+                            if row+x >= 0 and col+y >= 0 and row+x < len(self.model) and col+y < len(self.model[0]) and (self.boxes[(row+x)*len(self.model[0])+col+y][0], self.boxes[(row+x)*len(self.model[0])+col+y][1]) not in self.visited:
+                                pyautogui.click(self.boxes[(row+x)*len(self.model[0])+col+y][0], self.boxes[(row+x)*len(self.model[0])+col+y][1])
+                                # self.visited.add((self.boxes[(row+x)*len(self.model[0])+col+y][0], self.boxes[(row+x)*len(self.model[0])+col+y][1]))
+
+    def run(self) -> None:
+        """Runs the main program."""
+        self.initializeBoard()
+        self.startGame()
+        while self.mines > 0:
+            cur_mines = self.mines
+            self.getModel()
+            print(self.model)
+            self.placeObviousFlags()
+            self.clickEmptySquares()
+            
+            #if we couldn't advance the board/determine where any of the mines where then we need to break the loop and use dfs
+            if self.mines == cur_mines:
+                break
+            
+if __name__ == "__main__":
+    ai = Program()
+    ai.run()
+                            
